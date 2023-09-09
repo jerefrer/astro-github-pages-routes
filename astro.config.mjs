ab77bbcf601d74001d5d208c4a789e5f6c8559f1
Jérémy FRERE <frere.jeremy@gmail.com>
2023-09-09 15:42:34 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://jerefrer.github.io',
+  base: '/astro-github-pages-routes',
+});
